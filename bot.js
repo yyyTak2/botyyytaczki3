f3761fb48a5c187ee41d8db4b463fc136063c797
Dominik <dominik.cz89.com@gmail.com>
2019-03-11 13:35:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ bot.on('ready', () => {
     bot.user.setStatus('idle')
     bot.user.setPresence({
         game: {
-            name: '  ❗ NIE ZROBIEŚ ❗',
+            name: '    ❗ NIE ZROBIEŚ ❗',
             type: 0 
         }
     });
